4fe69b53a970be12e4641b5c8ff7da09351b325e
Ben Fulroth <fulrothb@icloud.com>
2019-12-11 16:59:59 -0500

    Used Django to create app drug_db

---
+++ drug_db/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DrugDbConfig(AppConfig):
+    name = 'drug_db'
---
+++ drug_db/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+from django.db.models import DateField, ForeignKey, IntegerField, CharField
+
+
+
