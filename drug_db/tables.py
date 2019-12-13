9fa860b1103c83fa5e693c9c1dbe12dfd97b9fb7
Ben Fulroth <fulrothb@icloud.com>
2019-12-13 13:39:12 -0500

    App drug_db: Added row id column to all tables

--- drug_db/tables.py
+++ drug_db/tables.py
@@ -3,13 +3,14 @@ from .models import BenchtopDrugLocations, BenchtopDrugSolubility
 
 
 class DrugLocationTable(tables.Table):
+    
     # Add a row number
     Row = tables.TemplateColumn("{{ row_counter }}")
 
     class Meta:
         model = BenchtopDrugLocations
         template_name = "django_tables2/semantic.html"
-        fields = ("Row", "broad_id", "barcode", "well", "plate", "conc_mM",
+        fields = ("Row", "id", "broad_id", "barcode", "well", "plate", "conc_mM",
                   "ori_vol_ul", "rem_vol_ul", "mw", "date_aliquoted")
 
 
@@ -21,4 +22,4 @@ class DrugSolubilityTable(tables.Table):
     class Meta:
         model = BenchtopDrugSolubility
         template_name = "django_tables2/semantic.html"
-        fields = ("Row", "broad_id", "buffer", "sol_um", "date", "source")
+        fields = ("Row", "id", "broad_id", "buffer", "sol_um", "date", "source")
